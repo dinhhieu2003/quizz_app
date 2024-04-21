ebbb20609760dc5e395876a86c4b340a91b90564
dinhhieu2003 <vuongdinhhieu2003@gmail.com>
2024-04-21 12:00:02 +0700

    CREATE: MainActivity

---
+++ app/src/main/java/com/example/quizz_app/view/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.quizz_app.view;
+
+import android.app.Dialog;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+
+import com.example.quizz_app.R;
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    private ImageButton imageButton;
+    private CircleImageView imageView;
+    public CircleImageView imageView1;
+    private FloatingActionButton floatingActionButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar =  findViewById(R.id.toolbar);
+        imageButton = findViewById(R.id.userImage2);
+        setSupportActionBar(toolbar);
+        imageView = findViewById(R.id.card1);
+        floatingActionButton = findViewById(R.id.chatHead);
+
+        //fragment for term & conditions!
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BottomSheetDialogFragment sheetFragment = new BottomSheetDialogFragment();
+                sheetFragment.show(getSupportFragmentManager(),sheetFragment.getTag());
+            }
+        });
+
+        DrawerLayout drawer =  findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
+    }
+}
